79c9102e93bc5ac6babb90b879f5e79660b2b5d1
Tino Gustin <gustintino@gmail.com>
2021-10-05 12:17:29 +0200

    Added countdown and shutdown

---
+++ Shutdown timer/Shutdown timer/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Shutdown_timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.seconds = new System.Windows.Forms.NumericUpDown();
+            this.minutes = new System.Windows.Forms.NumericUpDown();
+            this.hours = new System.Windows.Forms.NumericUpDown();
+            this.countdown_start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.seconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(285, 114);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(277, 86);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Specifc time";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.seconds);
+            this.tabPage2.Controls.Add(this.minutes);
+            this.tabPage2.Controls.Add(this.hours);
+            this.tabPage2.Controls.Add(this.countdown_start);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(277, 86);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Countdown";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "h";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // seconds
+            // 
+            this.seconds.Location = new System.Drawing.Point(179, 22);
+            this.seconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.seconds.Name = "seconds";
+            this.seconds.Size = new System.Drawing.Size(43, 23);
+            this.seconds.TabIndex = 10;
+            this.seconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.seconds.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.seconds.ValueChanged += new System.EventHandler(this.seconds_ValueChanged);
+            // 
+            // minutes
+            // 
+            this.minutes.Location = new System.Drawing.Point(109, 22);
+            this.minutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(43, 23);
+            this.minutes.TabIndex = 9;
+            this.minutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.minutes.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.minutes.ValueChanged += new System.EventHandler(this.minutes_ValueChanged);
+            // 
+            // hours
+            // 
+            this.hours.Location = new System.Drawing.Point(39, 22);
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(43, 23);
+            this.hours.TabIndex = 8;
+            this.hours.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.hours.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.hours.ValueChanged += new System.EventHandler(this.hours_ValueChanged);
+            // 
+            // countdown_start
+            // 
+            this.countdown_start.Location = new System.Drawing.Point(225, 57);
+            this.countdown_start.Name = "countdown_start";
+            this.countdown_start.Size = new System.Drawing.Size(46, 23);
+            this.countdown_start.TabIndex = 7;
+            this.countdown_start.Text = "Start";
+            this.countdown_start.UseVisualStyleBackColor = true;
+            this.countdown_start.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "m";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "s";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.seconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button countdown_start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown hours;
+        private System.Windows.Forms.NumericUpDown seconds;
+        private System.Windows.Forms.NumericUpDown minutes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Shutdown timer/Shutdown timer/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shutdown_timer
+{
+    public partial class Form1 : Form
+    {
+        /* - fix resizing after removing controls
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        private int s;
+        private int m;
+        private int h;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+
+            hours.Maximum = Int32.MaxValue;
+
+            hours.Controls[0].Visible = false;
+            minutes.Controls[0].Visible = false;
+            seconds.Controls[0].Visible = false;
+
+
+            //cause am idiot: https://stackoverflow.com/questions/5226688/numericupdown-mousewheel-event-increases-decimal-more-than-one-increment
+            hours.Increment = 1m / SystemInformation.MouseWheelScrollLines;
+            minutes.Increment = 1m / SystemInformation.MouseWheelScrollLines;
+            seconds.Increment = 1m / SystemInformation.MouseWheelScrollLines;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            s = (int)seconds.Value;
+            m = (int)minutes.Value;
+            h = (int)hours.Value;
+
+            timer1.Start();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+             if(s > 0)
+             {
+                s--;
+             }
+             else
+             {
+                if(m > 0)
+                {
+                    m--;
+
+                    s = 59;
+                }
+                else
+                {
+                    if(h > 0)
+                    {
+                        h--;
+
+                        m = 59;
+                        s = 59;
+                    }
+                }
+             }
+
+            hours.Value = h;
+            minutes.Value = m;
+            seconds.Value = s;
+
+            if(h == 0 && m == 0 && s == 0)
+            {
+                timer1.Stop();
+                System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+            }
+        }
+
+        private void hours_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minutes_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seconds_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
